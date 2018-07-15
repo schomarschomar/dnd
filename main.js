521b7ea0cf9a6e8229e1f1b2a0b056f19fb79e35
schomarschomar <markus.schoupal@gmail.com>
2018-07-15 14:24:34 +0200

    Update main.js

--- main.js
+++ main.js
@@ -54,7 +54,7 @@ myFunction();
 }
 
 function deleteCookie1(cname) {
-document.cookie = cname + "=;" + " expires=Thu, 01 Jan 1970 00:00:00 UTC; expires + ";path=/";
+document.cookie = cname + "=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/";
 myFunction();
 }
 
